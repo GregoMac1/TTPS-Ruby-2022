f83246d68eb03162a463cedebe49283d0f6b7136
Gregorio Maclen <gregorio.maclen@gmail.com>
2022-09-16 16:03:33 -0300

    .

---
+++ practica-2/p2ej6.rb
@@ -0,0 +1,58 @@
+class Documento
+  attr_accessor :creador, :contenido, :publico, :borrado
+
+  def initialize(usuario, publico = true, contenido = '')
+    self.creador = usuario
+    self.publico = publico
+    self.contenido = contenido
+    self.borrado = false
+  end
+
+  def borrar
+    self.borrado = true
+  end
+
+  def puede_ser_visto_por?(usuario)
+    usuario.puede_ver? self
+  end
+
+  def puede_ser_modificado_por?(usuario)
+    usuario.puede_modificar? self
+  end
+
+  def puede_ser_borrado_por?(usuario)
+    usuario.puede_borrar? self
+  end
+end
+
+class Lector
+  attr_accessor :usuario, :mail, :clave
+
+  def initialize(usuario, mail, clave)
+    self.usuario = usuario
+    self.mail = mail
+    self.clave = clave
+  end
+
+  def puede_ver?(documento)
+    documento.publico
+  end
+
+  def puede_modificar?(documento)
+    false
+  end
+
+  def puede_borrar?(documento)
+    false
+  end
+end
+
+class Redactor < Lector
+  def puede_modificar?(documento)
+    documento.creador == self
+  end
+end
+
+class Director < Redactor
+
+end
